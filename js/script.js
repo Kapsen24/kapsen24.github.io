cdfde7f65a547fe134a9b2bf0c3e588fddb24cb6
Dani <dahhwe@gmail.com>
2023-07-22 13:11:11 +0700

    Website beta
    First look my guys.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openSidePanel() {
+  const sidePanel = document.getElementById('sidePanel');
+  sidePanel.style.right = '0';
+}
+
+function closeSidePanel() {
+  const sidePanel = document.getElementById('sidePanel');
+  sidePanel.style.right = '-300px';
+}
+
+
