b668abb812d67198e3d1ae32f0c04e7919f44eca
Dani <dahhwe@gmail.com>
2023-07-23 16:17:00 +0700

    Sidebar now works. Minor adjustments.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,29 @@ function openSidePanel() {
     sidePanel.style.right = '0';
 }
 
+// function closeSidePanel() {
+//     const sidePanel = document.getElementById('sidePanel');
+//     sidePanel.style.right = '-300px';
+// }
+
+// Function to close the sidebar when the close button is clicked
 function closeSidePanel() {
-    const sidePanel = document.getElementById('sidePanel');
-    sidePanel.style.right = '-300px';
+    document.getElementById('sidePanel').style.right = '-300px';
+}
+
+// Function to handle scrolling to the clicked section
+function scrollToSection(sectionId, offset = 70) {
+    // Close the sidebar
+    closeSidePanel();
+
+    // Scroll to the section with the specified ID and offset
+    const section = document.getElementById(sectionId);
+    if (section) {
+        const offsetPosition = section.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({
+            top: offsetPosition - offset,
+            behavior: 'smooth',
+        });
+    }
 }
 
